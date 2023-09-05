1df729f35cb00318289e5b59273e9e0f0b4fd541
23BThomas <bettysamthomas@outlook.com>
2023-09-05 09:11:59 -0400

    Changes made

---
+++ Maths/mathematics.py
@@ -0,0 +1,20 @@
+#add function
+def summation(a, b):
+    c = a + b
+    return c
+
+#a = input("Enter a number")
+#b = input("enter a number")
+
+#function subtraction
+def subtraction( a, b):
+    d = a - b
+    return d
+
+def multiplication(a, b):
+    e = a * b
+    return e
+
+
+
+    
